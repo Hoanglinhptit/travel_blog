4e175d34a360c324acebfdc787164da26e99b47a
Hoang Linh <linh.lh@tinasoft.vn>
2023-08-26 14:17:28 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import http from "http";
+// import { createClient } from "@libsql/client";
 import "dotenv/config";
 import routes from "./routes";
 import { errorHandeler } from "./middlewares/ErrorHandler";
@@ -9,6 +10,11 @@ import { DatabaseError } from "./middlewares/DatabaseError";
 if (!process.env.TOKEN_SECRET) {
   throw new Error("TOKEN_SECRET must be set in .env file");
 }
+
+// const client = createClient({
+//   url: "libsql://your-database.turso.io",
+//   authToken: "your-auth-token",
+// });
 
 const app = express();
 const port = 3000;
@@ -31,7 +37,9 @@ app.get("/", (req: Request, res: Response) => {
 app.all("/*", () => {
   throw new BadRouteError();
 });
+
 app.use(errorHandeler);
+
 server.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
