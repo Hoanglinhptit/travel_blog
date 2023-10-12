989e1bfe6331cfbb824ed4d56cf4ed14d60b1014
Hoanglinhptit <lehoanglinhptit@gmail.com>
2023-10-12 07:11:00 +0700

    update connection pool and scenarios

--- dist/src/app.js
+++ dist/src/app.js
@@ -8,11 +8,10 @@ const cors_1 = __importDefault(require("cors"));
 const http_1 = __importDefault(require("http"));
 require("dotenv/config");
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
-const body_parser_1 = __importDefault(require("body-parser"));
+// import bodyParser from "body-parser";
 const aws_sdk_1 = __importDefault(require("aws-sdk"));
 const routes_1 = __importDefault(require("./routes"));
 const ErrorHandler_1 = require("./middlewares/ErrorHandler");
-const BadRouteHandler_1 = require("./middlewares/BadRouteHandler");
 // import { DatabaseError } from "./middlewares/DatabaseError";
 if (!process.env.TOKEN_SECRET) {
     throw new Error("TOKEN_SECRET must be set in .env file");
@@ -20,19 +19,16 @@ if (!process.env.TOKEN_SECRET) {
 aws_sdk_1.default.config.update({
     accessKeyId: "AKIAW7VUGLFTOU63FWU6",
     secretAccessKey: "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",
-    // region: "US East (N. Virginia) us-east-1",
 });
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.use(body_parser_1.default.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use((0, express_fileupload_1.default)({ useTempFiles: true, tempFileDir: "/tpm/" }));
 const port = 3000;
 const server = http_1.default.createServer(app);
 (0, routes_1.default)(app);
-app.all("/*", () => {
-    throw new BadRouteHandler_1.BadRouteError();
-});
+app.get("/", (req, res) => res.send("Hello Mai Meo"));
 app.use(ErrorHandler_1.errorHandeler);
 server.listen(port, () => {
     console.log(`http://localhost:${port}`);
